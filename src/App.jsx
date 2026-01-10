b99aad37506f2f113995901b3627a0e917eb17ea
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2026-01-10 11:16:15 +0100

    Pagination addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import { useState } from 'react';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+
+//Pages
+const Home = React.lazy(() => import("./pages/Home"));
+
+//Components
+const Navbar = React.lazy(() => import("./components/Navbar"));
+const Footer = React.lazy(() => import("./components/Footer"));
 
 function App() {
   
